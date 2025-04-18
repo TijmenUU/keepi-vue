e9845f9b73e3748a8d60af2da6b857094731a124
Tijmen van Nesselrooij <tijmen_222@hotmail.com>
2025-04-18 11:29:33 +0200

    Remove jittery space responsiveness

--- src/components/NavigationMenu.vue
+++ src/components/NavigationMenu.vue
@@ -13,14 +13,14 @@ import MenuLink from "@/components/MenuLink.vue";
       >
         <img src="/keepi.svg" class="" alt="Keepi Logo" />
         <span
-          class="hidden h-8 self-center whitespace-nowrap text-2xl font-semibold text-white sm:block"
+          class="hidden h-8 self-center text-2xl font-semibold whitespace-nowrap text-white sm:block"
           >Keepi</span
         >
       </router-link>
 
       <div class="block w-auto" id="navbar-default">
         <div
-          class="mt-0 flex flex-row space-x-3 rounded-lg border-0 border-gray-700 bg-gray-900 p-0 font-medium sm:space-x-8 rtl:space-x-reverse"
+          class="mt-0 flex flex-row space-x-5 rounded-lg border-0 border-gray-700 bg-gray-900 p-0 font-medium rtl:space-x-reverse"
         >
           <MenuLink route="/"> Invoer </MenuLink>
           <MenuLink route="/categories"> Categorieën </MenuLink>
