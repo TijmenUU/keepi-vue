ff5358ce7c0ae3467ee7716f87a381550639f846
Tijmen van Nesselrooij <tijmen_222@hotmail.com>
2025-04-18 11:23:03 +0200

    Move loader to separate component

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import KeepiLoader from "@/components/KeepiLoader.vue";
 import NavigationMenu from "@/components/NavigationMenu.vue";
 import { router } from "@/router";
 import { useApplicationStore } from "@/store/application-store";
@@ -33,7 +34,7 @@ const buildCommit: string = import.meta.env.VITE_APPLICATION_BUILD_COMMIT;
 
           <template #fallback>
             <div class="flex flex-grow flex-col items-center justify-center">
-              <div class="loader"></div>
+              <KeepiLoader />
             </div>
           </template>
         </Suspense>
@@ -66,40 +67,5 @@ const buildCommit: string = import.meta.env.VITE_APPLICATION_BUILD_COMMIT;
 .fade-enter-from,
 .fade-leave-to {
   opacity: 0;
-}
-
-.loader {
-  height: 15px;
-  aspect-ratio: 4;
-  --_g: no-repeat radial-gradient(farthest-side, #eee 90%, #0000);
-  background:
-    var(--_g) left,
-    var(--_g) right;
-  background-size: 25% 100%;
-  display: grid;
-}
-.loader:before,
-.loader:after {
-  content: "";
-  height: inherit;
-  aspect-ratio: 1;
-  grid-area: 1/1;
-  margin: auto;
-  border-radius: 50%;
-  transform-origin: -100% 50%;
-  background: #eee;
-  animation: l49 1s infinite linear;
-}
-.loader:after {
-  transform-origin: 200% 50%;
-  --s: -1;
-  animation-delay: -0.5s;
-}
-
-@keyframes l49 {
-  58%,
-  100% {
-    transform: rotate(calc(var(--s, 1) * 1turn));
-  }
 }
 </style>
---
+++ src/components/KeepiLoader.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="loader"></div>
+</template>
+
+<style scoped>
+.loader {
+  height: 15px;
+  aspect-ratio: 4;
+  --_g: no-repeat radial-gradient(farthest-side, #eee 90%, #0000);
+  background:
+    var(--_g) left,
+    var(--_g) right;
+  background-size: 25% 100%;
+  display: grid;
+}
+.loader:before,
+.loader:after {
+  content: "";
+  height: inherit;
+  aspect-ratio: 1;
+  grid-area: 1/1;
+  margin: auto;
+  border-radius: 50%;
+  transform-origin: -100% 50%;
+  background: #eee;
+  animation: l49 1s infinite linear;
+}
+.loader:after {
+  transform-origin: 200% 50%;
+  --s: -1;
+  animation-delay: -0.5s;
+}
+
+@keyframes l49 {
+  58%,
+  100% {
+    transform: rotate(calc(var(--s, 1) * 1turn));
+  }
+}
+</style>
